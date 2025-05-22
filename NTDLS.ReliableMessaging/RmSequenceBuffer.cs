51bacb43a321a87b795e8d44c46b9040df5eecd1
Josh Patterson <Josh@NTDLS.com>
2025-05-22 14:14:49 -0400

    Sequence buffer.

---
+++ NTDLS.ReliableMessaging/RmSequenceBuffer.cs
@@ -0,0 +1,113 @@
+﻿namespace NTDLS.ReliableMessaging
+{
+    /// <summary>
+    /// Provides a buffer for out-of-order packets.
+    /// This is used to ensure that packets are processed in the correct order.
+    /// </summary>
+    /// <typeparam name="T">Type of data in the packet</typeparam>
+    public class RmSequenceBuffer<T>
+    {
+        private readonly Dictionary<long, T> _buffer = new();
+
+        private long _lastConsumedSequence = -1;
+
+        /// <summary>
+        /// Represents a method that handles a buffer event by processing a sequence number and associated data.
+        /// </summary>
+        /// <param name="sequence">The sequence number of the buffer event.</param>
+        /// <param name="data">The data associated with the buffer event.</param>
+        public delegate void BufferSequenceHandler(long sequence, T data);
+
+        /// <summary>
+        /// Represents a method that handles a buffer event by processing a sequence number and associated data.
+        /// </summary>
+        /// <param name="data">The data associated with the buffer event.</param>
+        public delegate void BufferHandler(T data);
+
+        /// <summary>
+        /// Clears all items from the buffer and resets the last consumed sequence.
+        /// </summary>
+        /// <remarks>This method removes all elements from the buffer and sets the last consumed sequence
+        /// to -1. It is thread-safe and ensures that the operation is performed atomically.</remarks>
+        public void Clear()
+        {
+            lock (_buffer)
+            {
+                _buffer.Clear();
+                _lastConsumedSequence = -1;
+            }
+        }
+
+        /// <summary>
+        /// Processes a data packet by ensuring it is handled in the correct sequence order.
+        /// </summary>
+        /// <remarks>This method ensures that packets are processed in sequential order. If a packet
+        /// arrives out of order, it is stored in an internal buffer until all preceding packets have been processed.
+        /// Once the missing packets are received, the buffered packets are processed in sequence.</remarks>
+        /// <param name="sequence">The sequence number of the data packet. Must be greater than or equal to the last processed sequence number.</param>
+        /// <param name="data">The data associated with the packet to be processed.</param>
+        /// <param name="handler">A delegate that handles the processing of a data packet. The delegate is invoked with the sequence number
+        /// and data when the packet is ready to be processed in order.</param>
+        public void Process(long sequence, T data, BufferSequenceHandler handler)
+        {
+            lock (_buffer)
+            {
+                //The next packet in the sequence is the next one that needs to be sent. Flush it to the stream.
+                if (_lastConsumedSequence + 1 == sequence)
+                {
+                    _lastConsumedSequence = sequence;
+                    handler(sequence, data);
+                }
+                else
+                {
+                    //We received out-of-order packets. Store them in the buffer.
+                    _buffer.Add(sequence, data);
+                }
+
+                //Flush any packets that are now in order.
+                while (_buffer.TryGetValue(_lastConsumedSequence + 1, out var bytes))
+                {
+                    _buffer.Remove(_lastConsumedSequence + 1);
+                    _lastConsumedSequence++;
+                    handler(sequence, data);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Processes a data packet by ensuring it is handled in the correct sequence order.
+        /// </summary>
+        /// <remarks>This method ensures that packets are processed in sequential order. If a packet
+        /// arrives out of order, it is stored in an internal buffer until all preceding packets have been processed.
+        /// Once the missing packets are received, the buffered packets are processed in sequence.</remarks>
+        /// <param name="sequence">The sequence number of the data packet. Must be greater than or equal to the last processed sequence number.</param>
+        /// <param name="data">The data associated with the packet to be processed.</param>
+        /// <param name="handler">A delegate that handles the processing of a data packet. The delegate is invoked with
+        /// data when the packet is ready to be processed in order.</param>
+        public void Process(long sequence, T data, BufferHandler handler)
+        {
+            lock (_buffer)
+            {
+                //The next packet in the sequence is the next one that needs to be sent. Flush it to the stream.
+                if (_lastConsumedSequence + 1 == sequence)
+                {
+                    _lastConsumedSequence = sequence;
+                    handler(data);
+                }
+                else
+                {
+                    //We received out-of-order packets. Store them in the buffer.
+                    _buffer.Add(sequence, data);
+                }
+
+                //Flush any packets that are now in order.
+                while (_buffer.TryGetValue(_lastConsumedSequence + 1, out var bytes))
+                {
+                    _buffer.Remove(_lastConsumedSequence + 1);
+                    _lastConsumedSequence++;
+                    handler(data);
+                }
+            }
+        }
+    }
+}
